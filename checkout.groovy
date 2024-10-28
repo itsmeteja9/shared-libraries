d7221bb4e5d7d1ed55b34f7df0a95b64650811e7
itsmeteja9 <parvathaneniteja9@gmail.com>
2024-10-28 12:04:19 -0500

    test commit

--- checkout.groovy
+++ checkout.groovy
@@ -1,3 +1,3 @@
 def call(String repoUrl, String repobranch ) {
-     git branch: repobranch, credentialsId: '1e0cb709-2b07-4efd-aae1-42dbeff9260e', url: $repoUrl
+     git branch: ${repobranch}, credentialsId: '1e0cb709-2b07-4efd-aae1-42dbeff9260e', url: $repoUrl
 }
