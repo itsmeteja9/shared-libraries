eebbaf322abdd8d8c08fdafb93e303f063a48bdb
itsmeteja9 <55877527+itsmeteja9@users.noreply.github.com>
2024-10-28 14:14:38 -0500

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,3 +1,3 @@
-def call(String repoUrl, String branch ) {
-     git branch: $branch, credentialsId: '1e0cb709-2b07-4efd-aae1-42dbeff9260e', url: $repoUrl
+def call(String repoUrl, String repobranch ) {
+     git branch: repobranch, credentialsId: '1e0cb709-2b07-4efd-aae1-42dbeff9260e', url: repoUrl
 }
